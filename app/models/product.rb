dc4488a0107cba3a5694b21c0eb0e74472be212b
Qingqing <renwq8@126.com>
2017-02-25 12:03:14 +0800

    add search function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+    def self.search(search)
+      where("title LIKE ?", "%#{search}%").or(where("description LIKE ?", "%#{search}%")).or(where("kind LIKE ?", "%#{search}%"))
+   end
 
 end
